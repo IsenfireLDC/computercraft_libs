299640a785ff54394e99b81000bce5b449875f0f
IsenfireLDC <isenfireldc@gmail.com>
2025-04-12 19:22:42 -0500

    Remove debug processes from startup

--- kernel/startup.lua
+++ kernel/startup.lua
@@ -5,22 +5,6 @@ if not fs.exists("/init.lua") then
 	return
 end
 
-kernel.start(function()
-	while true do
-		local event = kernel.wait(nil, 'kernel', 'process_complete')
-
-		print("Process "..event[3].." "..event[4]..":", event[5] and event[5] or '')
-	end
-end)
-
-kernel.start(function()
-	while true do
-		local event = kernel.wait(nil, 'kernel', 'device')
-
-		print("Device "..event[3].."ed on side "..event[4].." with type(s):", table.unpack(event[5]))
-	end
-end)
-
 kernel.exec("/init.lua")
 
-kernel.run()
+print(kernel.run())
